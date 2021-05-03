e3a0e5787f9051d1858ec4df66c3d14093d61aee
Dylan Rush <81250088+rushdylan-psi@users.noreply.github.com>
2021-05-03 10:47:15 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+var menu = document.getElementById('conversion-select');
+btn.addEventListener('click', generateTorque);
 
-btn.addEventListener('click', myFunction);
-
-function myFunction (ev){
-    ev.preventDefault(); //prevent default submit
-    var N = document.getElementById("inputFtlb").value
-    document.getElementById("output").innerHTML = N * 12;
+function generateTorque(event) {
+    event.preventDefault();
+    var result = 0;
+    var inputVal = document.getElementById('inputFtlb').value;
+    if (menu.value == '1') {     
+      result = inputVal * 12;
+    } else if (menu.value == '2') {
+      result = inputVal * 1.3;
+      //alert(inputVal + ' is equal to ' + result);
+    }
+    document.getElementById('output').innerHTML = inputVal + ' Foot pounds is equal to ' + result + ' inch pounds.';
 }
 
-
+// 5 ft lbs is equal to 60 in lbs
+// 5 ft lbs is equal to 6.5 nm
 
 
 
