2f7895273dcf32442af96a2553997ffb5e5495e2
Dylan Rush <81250088+rushdylan-psi@users.noreply.github.com>
2021-05-03 10:47:51 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ function generateTorque(event) {
       result = inputVal * 12;
     } else if (menu.value == '2') {
       result = inputVal * 1.3;
-      //alert(inputVal + ' is equal to ' + result);
     }
     document.getElementById('output').innerHTML = inputVal + ' Foot pounds is equal to ' + result + ' inch pounds.';
 }
 
-// 5 ft lbs is equal to 60 in lbs
-// 5 ft lbs is equal to 6.5 nm
+
 
 
 
